4b794dae4103823a7cbd88ad3d5931029eb92e84
AdrianBelciuganu <adrian.belciuganu@gmail.com>
2017-12-17 23:01:41 +0200

    edit list

--- view/src/test/java/com/fii/ai/view/ViewTests.java
+++ view/src/test/java/com/fii/ai/view/ViewTests.java
@@ -1,5 +1,6 @@
 package com.fii.ai.view;
 
+import com.fii.ai.view.DTO.Word;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -10,6 +11,22 @@ public class ViewTests {
     @Test
     public void test1(){
         View view = new View();
+    }
+
+    @Test
+    public void test2(){
+        Word word1 = new Word("Americana","cea mai buna pizza",10);
+        Word word3 = new Word("America","USA",150);
+        View exemplu = new View();
+        try {
+            exemplu.add(word1);
+            exemplu.add(word3);
+            for(Word i:exemplu.getWords())
+                System.out.println(i.getNormalizeForm() +' ' + i.getTextForm() + ' '+ i.getOffset());
+        }catch (Exception e){
+            e.printStackTrace();
+            throw e;
+        }
     }
 }
 
