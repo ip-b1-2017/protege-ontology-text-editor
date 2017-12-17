c2656e4bf350de79a7ebed2ad0f69d803ef98dc0
AdrianBelciuganu <adrian.belciuganu@gmail.com>
2017-12-17 18:22:52 +0200

    Verificare concept + teste axiome

--- view/src/test/java/com/fii/ai/view/ParserTests.java
+++ view/src/test/java/com/fii/ai/view/ParserTests.java
@@ -5,11 +5,11 @@ import org.apache.jena.ontology.OntModelSpec;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.junit.Test;
 import org.semanticweb.owlapi.apibinding.OWLManager;
-import org.semanticweb.owlapi.model.OWLOntology;
-import org.semanticweb.owlapi.model.OWLOntologyCreationException;
-import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.*;
 
 import java.io.File;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class ParserTests {
     @Test
@@ -19,8 +19,46 @@ public class ParserTests {
         // Now load the local copy
         OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
         OWLOntology localPizza = manager.loadOntologyFromOntologyDocument(file);
+
         System.out.println("Loaded ontology: " + localPizza);
+
+        Set<OWLAxiom> axioms = localPizza.getAxioms();
+        System.out.println(axioms);
+        //System.out.println(axioms.getClass());
+        //for(OWLAxiom a : axioms)
+        //    System.out.println(a);
+
+        Set<OWLClass> concepts = new LinkedHashSet<OWLClass>();
+        for(OWLClass cls : localPizza.getClassesInSignature()) {
+            concepts.add(cls);
+        }
+        String x = ("Veneziana" + ">").toLowerCase();
+        boolean check = false;
+        for(OWLClass concept : concepts) {
+            if((concept.toString().toLowerCase()).endsWith(x)) {
+                check = true;
+                System.out.println((concept.toString().toLowerCase()));
+                break;
+            }
+        }
+        System.out.println(check);
+
         OntModel m = ModelFactory.createOntologyModel( OntModelSpec.OWL_MEM );
 
     }
+
+    public boolean checkConcept(String x, OWLOntology ontology) {
+        Set<OWLClass> concepts = new LinkedHashSet<OWLClass>();
+        for(OWLClass cls : ontology.getClassesInSignature()) {
+            concepts.add(cls);
+        }
+        for(OWLClass concept : concepts) {
+            if((concept.toString().toLowerCase()).endsWith(x)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
+
+
