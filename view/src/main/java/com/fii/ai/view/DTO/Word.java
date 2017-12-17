99bafa86b795144779eb0a403506e47e68168893
andreichitic <andrei.chitic_22@yahoo.com>
2017-12-17 19:05:45 +0200

    added DTO's

--- view/src/main/java/com/fii/ai/view/DTO/Word.java
+++ view/src/main/java/com/fii/ai/view/DTO/Word.java
@@ -4,4 +4,43 @@ package com.fii.ai.view.DTO;
  * TODO:An word containing the form from the text, the normalized form and the offset
  */
 public class Word {
+    private String normalizeForm;
+    private  String textForm;
+    private  int offset;
+
+    public String getNormalizeForm() {
+        return normalizeForm;
+    }
+
+    public void setNormalizeForm(String normalizeForm) {
+        this.normalizeForm = normalizeForm;
+    }
+
+    public String getTextForm() {
+        return textForm;
+    }
+
+    public void setTextForm(String textForm) {
+        this.textForm = textForm;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+    public Word(String normalizeForm, String textForm, int offset) {
+
+        this.normalizeForm = normalizeForm;
+        this.textForm = textForm;
+        this.offset = offset;
+    }
+
+    public Word() {
+    }
+
+
 }
