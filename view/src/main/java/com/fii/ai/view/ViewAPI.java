42f8c0c25dfecbe231e02ecc951fb6784ff4de97
fenea <fenea95@gmail.com>
2017-12-19 01:09:34 +0200

    Changed the API, singe method call.

--- view/src/main/java/com/fii/ai/view/ViewAPI.java
+++ view/src/main/java/com/fii/ai/view/ViewAPI.java
@@ -10,22 +10,26 @@ import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
 
 import java.io.File;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ViewAPI {
-    public static List<Relation> getRelations(Word word){
+    public static List<Relation> getRelations(Word word, OWLOntology owlOntology, ArrayList<Word> words){
         //Load the ontology
         File file = new File("tmp/pizza.owl");
         //TODO LOAD Ontology from Protege
         OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-        OWLOntology owlOntology = null;
-        try {
-            owlOntology = manager.loadOntologyFromOntologyDocument(file);
-            System.out.println("Loaded ontology: " + owlOntology);
-        } catch (OWLOntologyCreationException e) {
-            throw new InternalException("Failed to load the ontology");
+        if(owlOntology == null){
+            try {
+                owlOntology = manager.loadOntologyFromOntologyDocument(file);
+                System.out.println("Loaded ontology: " + owlOntology);
+            } catch (OWLOntologyCreationException e) {
+                throw new InternalException("Failed to load the ontology");
+            }
         }
         View view = new View();
+        view.setWords(words);
         // TODO: Comply wih GUI format
         return view.getRelations(word,owlOntology);
     }
